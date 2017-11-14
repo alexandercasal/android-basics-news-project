fa6c2df4c0eba15ac15f5ba7a9b813e7cf2e13ec
Alexander Casal <alexandericasal@gmail.com>
2017-11-13 20:50:11 -0700

    Added Sort By to the settings fragment

--- app/src/main/java/com/alexander/udacity/technews/controller/SettingsFragment.kt
+++ app/src/main/java/com/alexander/udacity/technews/controller/SettingsFragment.kt
@@ -1,6 +1,15 @@
 package com.alexander.udacity.technews.controller
 
+import android.os.Bundle
 import android.preference.PreferenceFragment
+import com.alexander.udacity.technews.R
 
 class SettingsFragment : PreferenceFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.settings_main)
+
+        val sortByPreference = findPreference(getString(R.string.settings_sort_by_key))
+    }
 }
